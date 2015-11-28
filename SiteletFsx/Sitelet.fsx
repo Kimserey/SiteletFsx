31f7d9de09b7a5096b01e9749c3fd77076b9e7fc
Kimserey Lam <kimserey.lam@housing-insight.co.uk>
2015-11-28 01:39:14 +0000

    Added boostrap css resource and one RPC. Not working.

--- SiteletFsx/Sitelet.fsx
+++ SiteletFsx/Sitelet.fsx
@@ -7,24 +7,49 @@ open WebSharper
 open WebSharper.Sitelets
 open System.IO
 open Common
+open WebSharper.Resources
+
+module Resources =
+    
+    type BootstrapResource() =
+        inherit BaseResource("//maxcdn.bootstrapcdn.com/bootstrap/3.3.5", "css/bootstrap.min.css")
+
+module Server =
+    
+    [<Rpc>]
+    let getUsers (id: string): Async<string> =
+        async { 
+            return id + " hello"
+        }
 
 [<JavaScript>]
 module Client =
     open WebSharper.UI.Next
     open WebSharper.UI.Next.Html
     open WebSharper.UI.Next.Client
+    open Server
 
-
+    [<Require(typeof<Resources.BootstrapResource>)>]
     let main() = 
         let input = Var.Create ""
         let json = Json.Serialize "json test"
+        let username = Var.Create ""
 
         div [
-            h2 [text "Hello world"]
+            h2Attr [attr.``class`` "well"] [text "Hello world"]
             div [text "Write something here"]
+
             Doc.Input [] input
             div [Doc.TextView input.View]
+            
             Doc.TextNode json
+            
+            Doc.Button "Get user rpc" [] (fun () -> 
+                async {
+                    let! user = getUsers "x"
+                    do Var.Set username user
+                } |> Async.Start)
+            Doc.TextView username.View
         ]
 
 module Site =
@@ -48,5 +73,3 @@ module Site =
         {  Route = "test"
            Sitelet = sitelet
            Metadata = metadata }
-        
-  
