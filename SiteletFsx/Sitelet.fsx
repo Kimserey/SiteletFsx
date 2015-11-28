681b3e5e9887dcc7bac86e7d8d4bf46b4e29d610
Kimserey Lam <kimserey.lam@housing-insight.co.uk>
2015-11-28 16:34:31 +0000

    Move owin starter to fsx

--- SiteletFsx/Sitelet.fsx
+++ SiteletFsx/Sitelet.fsx
@@ -60,16 +60,65 @@ module Site =
     let site =
         Sitelet.Content "test" "test"  (fun _ -> Content.Page(Title = "Hello", Body = [client <@ Client.main () @>]))
 
+    
+    open global.Owin
+    open Microsoft.Owin.Hosting
+    open Microsoft.Owin.StaticFiles
+    open Microsoft.Owin.FileSystems
+    open WebSharper.Owin
+    open Common
+    open System.Threading.Tasks
+    open System.Collections.Generic
+
+    type SiteMiddleware(next: Owin.AppFunc, sitelet, meta, rootDir, ?debug) =
+        let exec =
+            let siteletMw =
+                WebSharper.Owin.SiteletMiddleware(
+                    next, 
+                    Options.Create(meta)
+                        .WithServerRootDirectory(rootDir)
+                        .WithDebug(defaultArg debug false),
+                    sitelet)
+            let staticFilesMw =
+                Microsoft.Owin.StaticFiles.StaticFileMiddleware(
+                    AppFunc siteletMw.Invoke,
+                    StaticFileOptions(
+                        FileSystem = PhysicalFileSystem(rootDir)))
+            staticFilesMw.Invoke
+
+        member this.Invoke(env: IDictionary<string, obj>) = exec env
+
     let main =
 
         let metadata = Common.WsCompiler.compileToWs @"C:\Projects\SiteletFsx\SiteletFsx\bin\Debug"
 
-        let sitelet = 
-            Sitelet.Sum [
-                site
-                PageOneSite.site
-            ]
+        let sitelet = site
+//            Sitelet.Sum [
+//                site
+//                PageOneSite.site
+//            ]
 
-        {  Route = "test"
-           Sitelet = sitelet
-           Metadata = metadata }
+//        {  Route = "test"
+//           Sitelet = sitelet
+//           Metadata = metadata }
+
+
+    
+        use server = WebApp.Start("http://localhost:9091", fun appB ->
+
+            appB.Use(Owin.MidFunc(fun next -> 
+                let mw = SiteMiddleware(next, sitelet, metadata, @"C:\Projects\SiteletFsx\SiteletFsx\bin\Debug")
+                Owin.AppFunc mw.Invoke)) |> ignore
+                        
+            appB.Use(fun ctx next ->
+                    Task.Factory.StartNew(fun () ->
+                        let s : Stream =
+                            ctx .Set("owin.ResponseStatusCode", 404)
+                                .Set("owin.ResponseReasonPhrase", "Not Found")
+                                .Get("owin.ResponseBody")
+                        use w = new StreamWriter(s)
+                        w.Write("Page not found"))) |> ignore
+        )
+                
+        stdout.WriteLine("Serving {0}", "http://localhost:9091")
+        stdin.ReadLine() |> ignore
