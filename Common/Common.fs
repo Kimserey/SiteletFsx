bde914bdf302932aa1453cf2974a5f9ff61860ca
Kimserey Lam <kimserey.lam@housing-insight.co.uk>
2015-12-02 16:00:03 +0000

    Removed comment

--- Common/Common.fs
+++ Common/Common.fs
@@ -10,13 +10,6 @@ type CompiledSitelet = {
     Metadata: Core.Metadata.Info
 }
 
-//type SiteletPages = {
-//    Pages: List<Route * PageContent>
-//    Metadata: Core.Metadata.Info
-//}
-//and Route = string
-//and PageContent = Async<Content<string>>
-
 type Features = {
     Pages: List<string * Doc>
     Metadata: Core.Metadata.Info
